70b2dc7bcb6d18d5c3ae81564ebcb5903d544b87
Eduarda Timóteo <eduarda.silva7739@gmail.com>
2025-05-24 18:39:51 -0300

    Update desafio12.py

--- desafio12.py
+++ desafio12.py
@@ -1,5 +1,42 @@
+#desconto de 5%
 produto = float(input('Qual é o preço do produto? R$ '))
 
 desconto = produto - (produto * 5 / 100)
 
 print (f'O produto que custava R${produto}, na promoção com desconto de 5% irá custar R${desconto:.2f} ')
+
+
+#desconto de 10%
+produto = float(input('Qual é o preço do produto? R$ '))
+
+desconto = produto - (produto * 10 / 100)
+
+print (f'O produto que custava R${produto}, na promoção com desconto de 10% irá custar R${desconto:.2f} ')
+
+
+#desconto de 15%
+produto = float(input('Qual é o preço do produto? R$ '))
+
+desconto = produto - (produto * 15 / 100)
+
+print (f'O produto que custava R${produto}, na promoção com desconto de 15% irá custar R${desconto:.2f} ')
+
+
+#desconto de 50%
+produto = float(input('Qual é o preço do produto? R$ '))
+
+desconto = produto - (produto * 50 / 100)
+
+print (f'O produto que custava R${produto}, na promoção com desconto de 50% irá custar R${desconto:.2f} ')
+
+
+#produto = float(input('Qual é o preço do produto? R$ '))
+#desconto_percentual = float(input('Qual é o percentual de desconto? (%) '))
+
+#desconto_valor = produto * (desconto_percentual / 100)
+#preco_final = produto - desconto_valor
+
+#print(f'O produto que custava R${produto:.2f}, na promoção com {desconto_percentual:.0f}% de desconto, irá custar R${preco_final:.2f}.')
+
+
+
